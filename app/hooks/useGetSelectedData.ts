aae11db9211cad98c29b12d95721cd1bde457145
1F10220149 <s1F102201492@iniad.org>
2025-08-16 13:39:32 +0900

    prettier導入

--- app/hooks/useGetSelectedData.ts
+++ app/hooks/useGetSelectedData.ts
@@ -1,61 +1,60 @@
-import { useState } from "react"
+import { useState } from "react";
 import { popuDataModel, prefDataModel } from "../models/Model";
 
 // 選択された都道府県の人口構成データを管理するカスタムフック
 const useGetSelectedData = () => {
+  // 人口構成のデータを取得する関数
+  const getPopuData = async (pc: number) => {
+    const res = await fetch("api/resas/getPopu", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify({ prefCode: pc }),
+    });
 
-    // 人口構成のデータを取得する関数
-    const getPopuData = async (pc: number) => {
+    const result = await res.json();
+    const data_final = result.data.result.data;
 
-        const res = await fetch('api/resas/getPopu', {
-            method: 'POST',
-            headers: {
-            'Content-Type': 'application/json; charset=UTF-8',
-            },
-            body: JSON.stringify({prefCode: pc})
-        })
-
-        const result = await res.json();
-        const data_final = result.data.result.data
-
-        if (!data_final) {
-            
-            return ;
-        }
-
-        console.log("人口構成: ", data_final)
-        return data_final;
+    if (!data_final) {
+      return;
     }
 
-    // 選択された都道府県の人口構成のデータを管理
-    const [selectedData, setSelectedData] = useState<Record<string, popuDataModel[]>>({})
+    console.log("人口構成: ", data_final);
+    return data_final;
+  };
 
-    // 都道府県の選択
-    const handlePrefAdd = async (data: prefDataModel) => {
-        if (!Object.keys(selectedData).includes(data.prefName)) {
-            // 県を追加したときの処理
-            const newPopuData = await getPopuData(data.prefCode);
+  // 選択された都道府県の人口構成のデータを管理
+  const [selectedData, setSelectedData] = useState<
+    Record<string, popuDataModel[]>
+  >({});
 
-            setSelectedData((prev) => {
-              return {...prev, [data.prefName]: newPopuData};
-            })
-        }
+  // 都道府県の選択
+  const handlePrefAdd = async (data: prefDataModel) => {
+    if (!Object.keys(selectedData).includes(data.prefName)) {
+      // 県を追加したときの処理
+      const newPopuData = await getPopuData(data.prefCode);
+
+      setSelectedData((prev) => {
+        return { ...prev, [data.prefName]: newPopuData };
+      });
     }
+  };
 
-    // 選択されている県を削除する機能
-    const handlePrefRemove = (removePrefName: string) => {
-        setSelectedData((prev) => {
-            const newPopuData: Record<string, popuDataModel[]> = {}
-            Object.keys(prev).map((key) => {
-              if (key !== removePrefName) {
-                newPopuData[key] = prev[key]
-              }
-            })
-            return newPopuData;
-          })
-    }  
+  // 選択されている県を削除する機能
+  const handlePrefRemove = (removePrefName: string) => {
+    setSelectedData((prev) => {
+      const newPopuData: Record<string, popuDataModel[]> = {};
+      Object.keys(prev).map((key) => {
+        if (key !== removePrefName) {
+          newPopuData[key] = prev[key];
+        }
+      });
+      return newPopuData;
+    });
+  };
 
-    return { selectedData, handlePrefAdd, handlePrefRemove }
-}
+  return { selectedData, handlePrefAdd, handlePrefRemove };
+};
 
-export default useGetSelectedData
+export default useGetSelectedData;
--- app/hooks/useShowSelector.ts
+++ app/hooks/useShowSelector.ts
@@ -1,13 +1,12 @@
 import { useState } from "react";
 
 const useShowSelector = () => {
+  // 県を選択するコンポーネントの表示を管理
+  const [showSelector, setShowSelector] = useState<boolean>(false);
 
-    // 県を選択するコンポーネントの表示を管理
-    const [showSelector, setShowSelector] = useState<boolean>(false);
+  const handleOpenClose = () => setShowSelector(!showSelector);
 
-    const handleOpenClose = () => setShowSelector(!showSelector);
+  return { showSelector, handleOpenClose };
+};
 
-  return { showSelector, handleOpenClose }
-}
-
-export default useShowSelector
+export default useShowSelector;
